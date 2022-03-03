c91cdc2b2aca6f62ebac4262bafd67080b1e684d
Ahmed <ahmedoneplusone@gmail.com>
2022-03-03 16:46:21 +0200

    added Property List

--- Front End/src/app/Property/property-list/property-list.component.ts
+++ Front End/src/app/Property/property-list/property-list.component.ts
@@ -7,6 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+
+  Properties:Array<any> = [
+    {
+    "ID":1,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":12000
+  },
+  {
+    "ID":2,
+    "Name":"Birla House 2",
+    "Type":"House",
+    "Price":11000
+  },
+  {
+    "ID":3,
+    "Name":"Birla House 3",
+    "Type":"House",
+    "Price":10000
+  },
+  {
+    "ID":4,
+    "Name":"Birla House 4",
+    "Type":"House",
+    "Price":15000
+  },
+  {
+    "ID":5,
+    "Name":"Birla House 5",
+    "Type":"House",
+    "Price":16000
+  },
+  {
+    "ID":6,
+    "Name":"Birla House 6",
+    "Type":"House",
+    "Price":19000
+  },
+]
+
   constructor() { }
 
   ngOnInit(): void {
