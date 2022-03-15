6a5091627efae83a5d8ce3b5908ed2fb186e8056
Ahmed <ahmedoneplusone@gmail.com>
2022-03-15 12:31:09 +0200

    changed add-prop reactive form to template driven form to fix issues

--- Front End/src/app/Property/add-property/add-property.component.ts
+++ Front End/src/app/Property/add-property/add-property.component.ts
@@ -1,10 +1,9 @@
- import { IProperty } from '../../model/IProperty'
-import { AlertifyService } from './../../services/alertify.service';
-import { HousingService } from './../../services/housing.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup ,FormControl,Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { NgForm } from '@angular/forms';
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
+import { IPropertyBase } from '../../model/IPropertyBase';
+
 
 @Component({
   selector: 'app-add-property',
@@ -12,67 +11,45 @@ import { TabsetComponent } from 'ngx-bootstrap/tabs';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-  @ViewChild('formTabs') formTabs:TabsetComponent;
-  addPropForm:FormGroup;
+  @ViewChild('Form') addPropertyForm: NgForm;
+  @ViewChild('formTabs') formTabs: TabsetComponent;
 
-  propertyTypes: Array<string> = ['House','Apartment','Villa','Duplex']
-  furnishTypes: Array<string> = ['Fully','Semi','Unfurnished']
+  // Will come from masters
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex']
+  furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished']
 
-  propertyView:IProperty ={
-    ID:null,
-    Name:'',
-    Price:null,
-    SellRent:null,
-    FType:null,
-    PType:null,
-    BHK:null,
-    City:null,
-    BuiltArea:null,
-    RTM:null,
-    Description:null
+  propertyView: IPropertyBase = {
+    ID: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    PType: null,
+    FType: null,
+    BHK: null,
+    BuiltArea: null,
+    City: null,
+    RTM: null
   };
 
-  constructor(private router:Router,private HousingService:HousingService,private AlertifyService:AlertifyService) { }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    this.addPropForm = new FormGroup({
-      propName: new FormControl(null,[Validators.required,Validators.minLength(5),Validators.maxLength(50)]),
-      propPType: new FormControl(null,[Validators.required,Validators.minLength(5),Validators.maxLength(50)]),
-      propPrice: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
-      propBHK: new FormControl(null,[Validators.required]),
-      propFType: new FormControl(null,[Validators.required]),
-      propCity: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(25)]),
-      propBuiltArea: new FormControl(null,[Validators.required,Validators.minLength(2),Validators.maxLength(25)]),
-      propSellRent: new FormControl(null,[Validators.required])
-
-    })
-
   }
 
-  get GetPropName(){
-    return this.addPropForm.get('propName') as FormControl
-  }
-  get GetPropType(){
-    return this.addPropForm.get('propType') as FormControl
-  }
-  get GetPropPrice(){
-    return this.addPropForm.get('propPrice') as FormControl
-  }
-  get GetCity(){
-    return this.addPropForm.get('propCity') as FormControl
+  onBack() {
+     this.router.navigate(['/']);
   }
 
-
-  OnSubmit(){
-    console.log(this.addPropForm);
-    this.addPropForm.markAllAsTouched();
-
+  onSubmit() {
+    console.log('Congrats, form Submitted');
+    console.log('SellRent=' + this.addPropertyForm.value.BasicInfo.SellRent);
+    console.log(this.addPropertyForm);
   }
+
 
   selectTab(tabId: number) {
-    if (this.formTabs?.tabs[tabId]) {
-      this.formTabs.tabs[tabId].active = true;
-    }
+    this.formTabs.tabs[tabId].active = true;
   }
 
 }
