2bb32c3c3543c0167c2e12cfae8beaa369fae3e0
Ahmed <ahmedoneplusone@gmail.com>
2022-03-24 14:35:17 +0200

    deployed to firebase

--- Front End/src/app/app.component.spec.ts
+++ Front End/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NewTestAng'`, () => {
+  it(`should have as title 'HSPA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NewTestAng');
+    expect(app.title).toEqual('HSPA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NewTestAng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('HSPA app is running!');
   });
 });
